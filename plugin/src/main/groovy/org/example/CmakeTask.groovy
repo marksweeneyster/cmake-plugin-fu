94d0ae9efceb3cc411092da29e3f30f8c4e31aaa
marksweeneyster <marksweeneyster@gmail.com>
2017-07-29 14:38:39 -0600

    very crude implementation hardcoded to run cmake with Ninja

--- plugin/src/main/groovy/org/example/CmakeTask.groovy
+++ plugin/src/main/groovy/org/example/CmakeTask.groovy
@@ -2,11 +2,24 @@ package org.example
 
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.Exec
 import org.gradle.process.internal.ExecAction
 
-class CmakeTask extends DefaultTask {
+class CmakeTask extends Exec {
     String greeting = 'hello from CmakeTask'
     String generator = 'Ninja'
+
+
+    public CmakeTask() {
+        executable 'cmake'
+
+        args = ['-G'+generator, "build/"];
+    }
+
+    @Override
+    protected void exec() {
+        super.exec()
+    }
 
     @TaskAction
     def cmake() {
